938d9abb019c3eccabd53c03cefa3f75722f7169
Mohamed hatem <113141680+microfavmoh@users.noreply.github.com>
2024-08-04 22:47:53 +0300

    Add files via upload

--- psp.py
+++ psp.py
@@ -1,80 +1,169 @@
+from tkinter import *
 import os
 import json
-import time
+import hashlib
 
-
-class username_password:
-    username=None
-    password=None
-
-
-def create_account():
-    username=None
-    while username==None:
-        username=str(input('please enter your username(note:username mustbe loger than 3 character but shorter than 15)'))
-    while len(username)<3 or len(username)>15:
-        username=str(input('please enter your username(note:username mustbe loger than 3 character but shorter than 15)'))
-    if os.path.exists("C:/Users/Arabtech/Desktop/Coding/users_data.json")==False:
+class main:
+    cwd=os.getcwd()
+    path_=os.path.join(cwd,'user_data.json')
+    screen=Tk()
+    screen.attributes("-fullscreen",True)
+    screen.title("place holder title")
+    font_=("Product Sans",10)
+    font_warning=("Product Sans",15,"bold")
+    username=Entry(screen)
+    password=Entry(screen)
+    text_username=Label(text="enter your username")
+    text_password=Label(text="enter your password")
+    text_username.configure(font=font_)
+    text_password.configure(font=font_)
+    text_username.place(relx=0.5,rely=0.2,anchor="center")
+    text_password.place(relx=0.5,rely=0.3,anchor="center")
+    username.place(relx=0.5,rely=0.24,anchor="center")
+    password.place(relx=0.5,rely=0.34,anchor="center")
+    def throw_warning(text_):
+        for widget in main.screen.winfo_children():
+            widgets=['.!entry','.!entry2','.!label','.!label2','.!button','.!button2']
+            if str(widget) not in widgets:
+                widget.destroy()
+            else:
+                pass  
+            warning_error=Label(text=text_)
+            warning_error.configure(font=main.font_warning,fg="red")
+            warning_error.place(relx=0.5,rely=0.45,anchor="center")
+    def text_encrypter(string):
+            string=string.encode("utf-8")
+            return hashlib.sha256(string).hexdigest()
+    def value_checker():
+        username=main.username
+        password=main.password
+        username=str(username.get())
+        password=str(password.get())
+        path_=main.path_
+        if len(username)==0:
+            main.throw_warning("enter your username")
+            return None
+        else:
             pass
-    elif os.path.exists("C:/Users/Arabtech/Desktop/Coding/users_data.json")==True:
-        with open('users_data.json','r') as file:
+
+        for char in username:
+            if char.isspace()==True:
+                main.throw_warning("username cannot contain spaces")
+                return None
+            else:
+                pass
+
+        if len(username)<3:
+            main.throw_warning("username cannot be shorter than three characters")
+            return None
+        else:
+            pass
+
+        if len(username)>15:
+            main.throw_warning("username cannot be longer than fifteen characters")
+            return None    
+        else:
+            pass
+
+        if os.path.exists(path_)==True:
+            with open(path_,'r') as file:
+                file=json.load(file)
+                if main.text_encrypter(username) in file:
+                    main.throw_warning("username already exists")
+                    return None
+                else:
+                    pass
+        else:
+            pass
+        if len(password)==0:
+            main.throw_warning("enter your password")
+            return None
+        else:
+            pass
+        for char in password:
+            if char.isspace()==True:
+                main.throw_warning("password cannot contain spaces")
+                return None
+            else:
+                pass
+        number_counter=0
+        for char in password:
+            if char.isnumeric()==True:
+                number_counter+=1
+                break
+            else:
+                pass
+        if number_counter==0:
+            main.throw_warning("password must contain atleast one number")
+            return None
+        else:
+            pass  
+        if len(password)<12:
+            main.throw_warning("password cannot be shorter than twelve characters")
+            return None
+        else:
+            pass
+        sum_upper=0
+        for i in password:
+            if i.isupper()==True:
+                sum_upper+=1
+            else:
+                pass
+        if sum_upper==0:
+            main.throw_warning("password must contain uppercase characters")
+            return None
+        else:
+            pass
+        sum_lower=0
+        for i in password:
+            if i.islower()==True:
+                sum_lower+=1
+            else:
+                pass
+        if sum_lower==0:
+            main.throw_warning("password must contain lowercase characters")
+            return None
+        else:
+            pass
+        if os.path.exists(path_)==False:
+            with open(path_,'w') as file:
+                user_data={main.text_encrypter(username):main.text_encrypter(password)}
+                file.write(json.dumps(user_data,indent=4))
+                file.close()
+        else:
+            with open(path_,'r+b') as file:
+                pass
+    button=Button(command=value_checker,background="yellow",text="Create account",bd=0)
+    button.configure(font=font_)
+    button.place(relx=0.5,rely=0.4,anchor="center")
+    def check():
+        username=main.username
+        password=main.password
+        username=username.get()
+        password=password.get()
+        with open(main.path_,'r') as file:
             file=json.load(file)
-            while username in file:
-                username=str(input("username already exists please try again"))
-            while len(username)<3 and len(username)>15:
-                username=str(input('please enter your username(note:username mustbe loger than 3 character but shorter than 15)'))
-
-    username_password.username=username
-    password=None
-    while password==None:
-        print('password must be atleast eight characters')
-        password=str(input('please enter a strong password'))
-    while len(password)<7:
-        password=str(input('please enter a strong password'))
-    with open('users_data.json',mode='a') as file:
-        user_data={username:password}
-        user_data=json.dumps(user_data,indent=4)
-        file.write(user_data)
-        file.close()
-
-
-def login():
-    if os.path.exists("C:/Users/Arabtech/Desktop/Coding/users_data.json")==True:
-        pass
-    else:
-        print('no accounts available please create an account')
-        time.sleep(2)
-        create_account()
-        return None
-    username_login=None
-    while username_login==None:
-        username_login=str(input('please enter your username'))
-    with open('users_data.json',mode='r') as file:
-        file=file.read()
-        if username_login in file:
-            pass
-        else:
-            while username_login not in file:
-                username_login=str(input('please enter your username'))
-    username_password.username=username_login
-    password_login=None
-    while password_login==None:
-        password_login=str(input('please enter your password'))
-    with open('users_data.json',mode='r') as file:
-        file=file.read()
-        if password_login in file:
-            pass
-        else:
-            while password_login not in file:
-                password_login=str(input('please enter your password'))
-
-
-user_input_create_account_login=None
-print("would you like to crete an account or login")
-time.sleep(2)
-while user_input_create_account_login not in ['account','login']:
-    user_input_create_account_login=input("if you want to create an account type 'account' and if you want to log in type 'login'.")
-if user_input_create_account_login=='account':
-    create_account()
-if user_input_create_account_login=='login':
-    login()
+            if main.text_encrypter(username) not in file:
+                main.throw_warning("username doesn't exsist")
+                return None
+            else:
+                pass
+            if main.text_encrypter(password)!=file.get(main.text_encrypter(username)):
+                main.throw_warning("iccorect password")
+                return None
+            else:
+                print("logged in")
+    def login():
+        destroyed_widgets=['.!button','.!button2']
+        for widget in main.screen.winfo_children():
+            if str(widget) in destroyed_widgets:
+                widget.destroy()
+            else:
+                pass
+        button=Button(command=main.check,background="yellow",text="Login",bd=0)
+        button.configure(font=main.font_)
+        button.place(relx=0.5,rely=0.4,anchor="center")
+    button_login=Button(command=login,background="yellow",text="Login",bd=0)
+    button_login.configure(font=font_)
+    button_login.place(relx=0.95,rely=0.04)
+main.screen.mainloop()

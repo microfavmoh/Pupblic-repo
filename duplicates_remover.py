848976b26386964d6a5c2e66868ab600618b06c4
Mohamed hatem <113141680+microfavmoh@users.noreply.github.com>
2024-09-25 15:51:23 +0300

    Add files via upload

--- duplicates_remover.py
+++ duplicates_remover.py
@@ -1,35 +1,22 @@
 import hashlib
 import os
-import time
 from tkinter.filedialog import askdirectory
 
-directory=askdirectory()
 hashes=set()
-non_hashable_files=[]
 
 def dup_remover(directory):
-    global hashes
     for filename in os.listdir(directory):
         path=os.path.join(directory, filename)
+        print(path)
         if os.path.isdir(path):
             dup_remover(path)
-        hash=hashlib.sha512(open(path,'rb').read()).digest()
+            continue
+        hash=hashlib.sha256(open(path,'rb').read()).digest()
         if hash not in hashes:
             hashes.add(hash)
         else:
             os.remove(path)
 
-    if not non_hashable_files:
-        print("the operation was completed sucessfuly")
-        pass
-    else:
-        time.sleep(2)
-        print("files that may still be duplicates:")
-        time.sleep(2)
-        for file in non_hashable_files:
-            print(file)
-            time.sleep(1)
-
-while not directory:
-    directory=askdirectory()
-dup_remover(directory)     
+directory=askdirectory()
+dup_remover(directory)
+print("the operation was completed sucessfuly")     
